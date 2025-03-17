c7855dcaffd1b4f3b341bcffeb60ed8accba7223
Bradley Rising-Tchernucha <bradders170402@gmail.com>
2025-03-17 12:20:51 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*
+const portfolioBtn=document.getElementById('portfolio-btn');
+const cafeBtn=document.getElementById('cafe-btn');
+const quizBtn=document.getElementById('quiz-btn');
+
+const portfolioContent=document.getElementById('portfolioContent');
+portfolioContent.style.display='none';
+const cafeContent=document.getElementById('cafeContent');
+cafeContent.style.display='none';
+const quizContent=document.getElementById('quizContent');
+quizContent.style.display='none';
+
+portfolioBtn.addEventListener('click',(event)=>{
+if(portfolioBtn(event.target)){portfolioContent.style.display='block';}else{portfolioContent.style.display='none'}
+});
+
+cafeBtn.addEventListener('click',(event)=>{
+    if(cafeBtn(event.target)){cafeContent.style.display='block';}else{cafeContent.style.display='none'}
+});
+
+quizBtn.addEventListener('click',(event)=>{
+    if(quizBtn(event.target)){quizContent.style.display='block';}else{quizContent.style.display='none'}
+});
+*/
